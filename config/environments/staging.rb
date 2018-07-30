c9dedd692419934deb8f306c6f6d1e73d7226902
Mark Rivera <Mrivera1991@gmail.com>
2018-07-30 03:04:15 +0000

    remove stuff

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -25,7 +25,4 @@ config.log_formatter = ::Logger::Formatter.new
 
 config.active_record.dump_schema_after_migration = false
 
-config.middleware.insert_before(::Rack::Runtime, "::Rack::Auth::Basic", "Staging") do |u, p|
-u == ENV["STAGING_USERNAME" ] && p == ENV["STAGING_PASSWORD"]
-end
 end
